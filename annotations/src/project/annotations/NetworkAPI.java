a9ad16e3bbcc492bfd9ca422b9031eb9774d377b
Christian Copeland <christian1jcope@gmail.com>
2025-02-10 09:18:05 -0500

    Working on Assignment 2 part 3

--- annotations/src/project/annotations/NetworkAPI.java
+++ annotations/src/project/annotations/NetworkAPI.java
@@ -2,7 +2,6 @@ package project.annotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
-
 @Target(ElementType.TYPE)
 public @interface NetworkAPI {
   // Marker annotation, should be applied to an interface type
--- annotations/src/project/annotations/NetworkAPIPrototype.java
+++ annotations/src/project/annotations/NetworkAPIPrototype.java
@@ -2,7 +2,7 @@ package project.annotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
-
+@NetworkAPI
 @Target(ElementType.METHOD)
 public @interface NetworkAPIPrototype {
 	// Marker annotation, should be applied to a method within a prototype class
