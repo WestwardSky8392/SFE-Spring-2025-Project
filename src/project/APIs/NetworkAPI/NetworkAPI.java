6223700cdb7ec2a1053b88f589b7a35fbf716952
Christian Copeland <christian1jcope@gmail.com>
2025-02-23 18:03:53 -0500

    Fixed(name again)

--- src/project/APIs/NetworkAPI/NetworkAPI.java
+++
@@ -1,9 +0,0 @@
-package networkapi;
-
-import datastorageapi.StorageComputeAPIPrototype;
-
-public interface NetworkAPI {
-    void askUser();
-    void sendData(String userInput, StorageComputeAPIPrototype dataStorage);
-    void implementsNewScreen(Screen screen);
-}
