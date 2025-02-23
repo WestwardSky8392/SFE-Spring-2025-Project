6bc954969c148d57b9d97b947dd84877851cb210
Csolitep <145181460+pokecedgo@users.noreply.github.com>
2025-02-23 17:39:30 -0500

    [Documentation Added] StorageComputeAPIPrototype.java

--- src/project/APIs/DataStorage/StorageComputeAPIPrototype.java
+++ src/project/APIs/DataStorage/StorageComputeAPIPrototype.java
@@ -3,14 +3,29 @@ package api.prototype;
 import api.StorageComputeAPI;
 import annotations.ProcessAPIPrototype; 
 
+/**
+ * Prototype implementation of StorageComputeAPI for testing and simulation.
+ */
 public class StorageComputeAPIPrototype implements StorageComputeAPI {
-    
+
+    /**
+     * Simulates reading data from a source.
+     * 
+     * @param source The source to read data from.
+     * @return Simulated data.
+     */
     @Override
     @ProcessAPIPrototype
     public String readData(String source) {
         return "Simulated data from " + source;
     }
 
+    /**
+     * Simulates writing data to a destination.
+     * 
+     * @param destination The location to store data.
+     * @param data The data to be stored.
+     */
     @Override
     @ProcessAPIPrototype
     public void writeData(String destination, String data) {
