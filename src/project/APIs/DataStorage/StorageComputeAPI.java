cdf782a4d9b7dbdf9849a6df0743fea23f1a6c09
Csolitep <145181460+pokecedgo@users.noreply.github.com>
2025-02-23 17:37:42 -0500

    [Fixed Documentation] StorageComputeAPI.java

--- src/project/APIs/DataStorage/StorageComputeAPI.java
+++ src/project/APIs/DataStorage/StorageComputeAPI.java
@@ -2,8 +2,26 @@ package api;
 
 import annotations.ProcessAPI; 
 
+/**
+ * StorageComputeAPI defines methods for reading and writing data 
+ * between different storage locations.
+ */
 @ProcessAPI
 public interface StorageComputeAPI {
+
+    /**
+     * Reads data from the specified source.
+     * 
+     * @param source The location to read data from.
+     * @return The retrieved data as a string.
+     */
     String readData(String source);
+
+    /**
+     * Writes data to the specified destination.
+     * 
+     * @param destination The location to store data.
+     * @param data The data to be written.
+     */
     void writeData(String destination, String data);
 }
