3df64ae11bbcc0f245be22e5adde270406f4d87e
Csolitep <145181460+pokecedgo@users.noreply.github.com>
2025-02-23 19:55:53 -0500

    New Src Directory

---
+++ src/project/annotations/NetworkAPI.java
@@ -0,0 +1,8 @@
+package annotations.src.project.annotations.NetworkAPI;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+@Target(ElementType.TYPE)
+public @interface NetworkAPI {
+
+}
---
+++ src/project/apis/networkapi/ValidInfo.java
@@ -0,0 +1,13 @@
+package annotations.src.project.annotations.NetworkAPI;
+
+import annotations.src.project.annotations.ProcessAPIPrototype;
+public class ValidInfo {
+    AskUser validInt;
+    public ValidInfo(AskUser validInt){
+        validInt = this.validInt;
+    }
+
+    public SendInfo sendToProcess() {
+        return null;
+    }
+}
