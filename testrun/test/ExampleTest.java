1f6fb9ed3271d15d5c779a51c5a37e428eae55bd
Antonio-Vav <tvscswimmer1@gmail.com>
2025-02-23 23:05:20 -0500

    test<datastorage>
    test implementation with mockito

---
+++ testrun/test/ExampleTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Test;
+import api.datastorage;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExampleTest {
+
+    @Test
+    public void testAddition() {
+        datastorage mockDataStore = mock(datastorage.class);
+        datastorage api1 = new datastorage(mockDataStore);
+        }
+    }
