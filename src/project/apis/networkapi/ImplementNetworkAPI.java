5d6f420506b05a02e5dd6793492b27a7c7768a64
Csolitep <145181460+pokecedgo@users.noreply.github.com>
2025-02-24 08:25:50 -0500

    [Added] ImplementNetworkAPI (#91)
    Done via pair programming (W/ Chris) Unable to access proper coding space so was done through computer-phone communication.

---
+++ src/project/apis/networkapi/ImplementNetworkAPI.java
@@ -0,0 +1,34 @@
+package project.apis.networkapi;
+
+import project.annotations.NetworkAPI;
+import project.apis.networkapi.Screen;
+import project.apis.networkapi.AskUser;
+import project.apis.networkapi.Window;
+import project.apis.networkapi.SendInfo;
+import project.apis.networkapi.ValidInfo;
+
+/**
+ * Prototype implementation of the NetworkAPI.
+ * This is a placeholder implementation that does not perform actual network operations.
+ */
+public class ImplementNetworkAPI implements Screen {
+
+    /**
+     * Displays a window for user interaction.
+     * Currently, this returns null as a placeholder.
+     * @param askUser User interaction request object.
+     * @return A Window instance (currently null).
+     */
+    @Override
+    public Window showWindow(AskUser askUser) {
+        return null; // Placeholder
+    }
+
+    /**
+     * Placeholder method to send information for processing.
+     * @return A SendInfo instance (currently null).
+     */
+    public SendInfo sendToProcess() {
+        return null;
+    }
+}
