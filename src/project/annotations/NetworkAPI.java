0bb7f9ba1c7df085c18203cb429de2bb8c7015e5
Csolitep <145181460+pokecedgo@users.noreply.github.com>
2025-02-23 19:28:52 -0500

    [Needed] src/project directory
    Issues with renaming the files to lowercase, republished directory along with fixed data-connections API folder

--- src/project/annotations/NetworkAPI.java
+++
@@ -1,8 +0,0 @@
-package annotations.src.project.annotations.NetworkAPI;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Target;
-@Target(ElementType.TYPE)
-public @interface NetworkAPI {
-
-}
