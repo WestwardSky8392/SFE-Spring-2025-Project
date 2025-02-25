30a8eac301be76dfd509cea81ba2fa18ae1b539b
Csolitep <145181460+pokecedgo@users.noreply.github.com>
2025-02-25 00:30:33 -0500

    Update DigitalRootPersistenceAPIPrototype.java (#136)

--- src/project/apis/computeapi/DigitalRootPersistenceAPIPrototype.java
+++ src/project/apis/computeapi/DigitalRootPersistenceAPIPrototype.java
@@ -1,3 +1,6 @@
+package project.apis.computeapi;
+
+import project.apis.computeapi.DigitalRootPersistenceAPI; 
 import project.annotations.ConceptualAPIPrototype;
 
 public class DigitalRootPersistenceAPIPrototype implements DigitalRootPersistenceAPI {
@@ -9,3 +12,4 @@ public class DigitalRootPersistenceAPIPrototype implements DigitalRootPersistenc
         return "Number " + number + " has a digital root persistence of 4 and final root 5";
     }
 }
+
