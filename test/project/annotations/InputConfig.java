f6e9b8a2c624d09a71bc3551b2b2ee754a8fd328
Csolitep <145181460+pokecedgo@users.noreply.github.com>
2025-02-25 12:56:01 -0500

    Create InputConfig.java (#153)

---
+++ test/project/annotations/InputConfig.java
@@ -0,0 +1,15 @@
+package test.project.annotations;
+
+import java.util.List;
+
+/**
+ * Mock interface representing an input configuration.
+ * This is created for testing purposes only.
+ */
+public interface InputConfig {
+    /**
+     * Retrieves the list of input values.
+     * @return A list of integers representing the input.
+     */
+    List<Integer> getInput();
+}
