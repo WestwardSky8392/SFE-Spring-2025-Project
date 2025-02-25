3de7b5163b39d103dc9d114f0fe81587af643c27
Csolitep <145181460+pokecedgo@users.noreply.github.com>
2025-02-25 13:04:07 -0500

    Update InMemoryOutputConfig.java

--- test/project/annotations/InMemoryOutputConfig.java
+++ test/project/annotations/InMemoryOutputConfig.java
@@ -1,27 +1,16 @@
 package test.project.annotations;
 
-import static org.mockito.Mockito.*;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Test-only implementation of the OutputConfig interface.
-
+ * Provides an in-memory list of strings to write output to.
  */
-public class InMemoryOutputConfig {
-
-    // Mock OutputConfig interface
-    public interface OutputConfig {
-        void writeOutput(String output);
-    }
-
+public class InMemoryOutputConfig implements OutputConfig {  // Implementing the OutputConfig interface
     private List<String> outputList;
 
     /**
+     * Constructor to initialize the in-memory output list.
      * @param outputList The list to store output strings.
      */
     public InMemoryOutputConfig(List<String> outputList) {
@@ -29,8 +18,10 @@ public class InMemoryOutputConfig {
     }
 
     /**
-     * Writes a string to the in-memory output list
+     * Writes a string to the in-memory output list.
+     * @param output The output string to be added.
      */
+    @Override
     public void writeOutput(String output) {
         outputList.add(output);
     }
