1cbef7a0c7b6c09b6c2af439fbd61de62ecda1b3
Csolitep <145181460+pokecedgo@users.noreply.github.com>
2025-02-25 12:56:40 -0500

    Create OutputConfig.java (#154)

---
+++ test/project/annotations/OutputConfig.java
@@ -0,0 +1,13 @@
+package test.project.annotations;
+
+/**
+ * Mock interface representing an output configuration.
+ * This is created for testing purposes only.
+ */
+public interface OutputConfig {
+    /**
+     * Writes the output string to the configured destination.
+     * @param output The output string to be written.
+     */
+    void writeOutput(String output);
+}
