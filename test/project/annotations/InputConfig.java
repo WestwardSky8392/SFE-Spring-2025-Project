7219e2f85937b42c1fa79c6735df30cc79142261
Csolitep <145181460+pokecedgo@users.noreply.github.com>
2025-02-26 00:20:54 -0500

    [Code Restructure] InputConfig.java
    Added proper imports and extra method class

--- test/project/annotations/InputConfig.java
+++ test/project/annotations/InputConfig.java
@@ -1,15 +1,14 @@
-package test.project.annotations;
+package project.annotations;
 
 import java.util.List;
+
 
 /**
  * Mock interface representing an input configuration.
  * This is created for testing purposes only.
  */
+
 public interface InputConfig {
-    /**
-     * Retrieves the list of input values.
-     * @return A list of integers representing the input.
-     */
-    List<Integer> getInput();
+    List<Integer> getInput(); 
+    String readInput();
 }
