f1d9ef09fd78643eb0688a2da46e9829a9d7e127
Csolitep <145181460+pokecedgo@users.noreply.github.com>
2025-02-26 00:27:10 -0500

    [Duplicate file] InMemoryInputConfig.java

--- test/project/annotations/InMemoryInputConfig.java
+++ test/project/annotations/InMemoryInputConfig.java
@@ -6,36 +6,6 @@ import java.util.List;
  * Test-only implementation of the InputConfig interface.
  * Provides an in-memory list of integers for input.
  */
-public class InMemoryInputConfig implements InputConfig {  // Implementing the InputConfig interface
-    private List<Integer> inputList;
-
-    /**
-     * Constructor to initialize the in-memory input list.
-     * @param inputList The list of integers to use as input.
-     */
-    public InMemoryInputConfig(List<Integer> inputList) {
-        this.inputList = inputList;
-    }
-
-    /**
-     * Retrieves the in-memory input list.
-     * @return The list of integers used for input.
-     */
-    @Override
-    public List<Integer> getInput() {
-        return inputList;
-    }
-}
-
-
-package project.annotations;
-
-import java.util.List;
-
-/**
- * Test-only implementation of the InputConfig interface.
- * Provides an in-memory list of integers for input.
- */
 public class InMemoryInputConfig implements InputConfig {
     private final List<Integer> inputList;
 
