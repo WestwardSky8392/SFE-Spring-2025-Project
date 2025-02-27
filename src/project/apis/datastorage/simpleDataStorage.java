6fa3db104395ed0d0edf552a24bd7fdad35a581d
Antonio-Vav <tvscswimmer1@gmail.com>
2025-02-27 17:31:33 +0000

    datastore implement

---
+++ src/project/apis/datastorage/simpleDataStorage.java
@@ -0,0 +1,20 @@
+public class SimpleDataStorage implements DataStorage {
+    @Override
+    public int[] readData(String location) throws Exception {
+        String[] stringData = location.split(",");
+        int[] data = new int[stringData.length];
+        for (int i = 0; i < stringData.length; i++) {
+            data[i] = Integer.parseInt(stringData[i].trim());
+        }
+        return data;
+    }
+
+    @Override
+    public void writeData(String location, int[] results) throws Exception {
+        System.out.println("Writing results to location " + location + ": ");
+        for (int result : results) {
+            System.out.print(result + " ");
+        }
+        System.out.println();
+    }
+}
