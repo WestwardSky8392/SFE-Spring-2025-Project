2c756cc6b2fd864d03708aae7b98289809ea5e8c
Csolitep <145181460+pokecedgo@users.noreply.github.com>
2025-02-26 22:53:15 -0500

    [ReAdded] ImplementDataStorage Componenet (#184)

--- src/project/apis/datastorage/ImplementDataStorage.java
+++ src/project/apis/datastorage/ImplementDataStorage.java
@@ -3,6 +3,9 @@ package project.apis.datastorage;
 import project.apis.datastorage.StorageComputeAPI;
 import project.apis.datastorage.DataStorageAPI; 
 import project.annotations.ProcessAPIPrototype;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * Implementation of the StorageComputeAPI.
@@ -54,6 +57,33 @@ public class ImplementDataStorage implements StorageComputeAPI {
     public void writeData(String destination, String data) {
         if (dataStorageAPI != null) {
             dataStorageAPI.storeData(destination, data);
+        }
+    }
+
+    /**
+     * Reads data from the given file path.
+     * @param filePath The file path to read data from.
+     * @return The data read from the file, or an empty string if an error occurs.
+     */
+    public String readFile(String filePath) {
+        try {
+            return new String(Files.readAllBytes(Paths.get(filePath)));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    /**
+     * Writes data to the given file path.
+     * @param filePath The file path to write data to.
+     * @param data The data to write.
+     */
+    public void writeFile(String filePath, String data) {
+        try {
+            Files.write(Paths.get(filePath), data.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
